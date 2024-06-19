301cd6dc59030f6d966a038f496be7f471a12626
GautamGirdhar <girdhargautam6@gmail.com>
2024-06-19 20:25:05 +0530

    Day51

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -24,12 +25,18 @@ app.get('/restaurants', function(req, res) {
   res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
 });
 
+app.get('/restaurants/:id', function(req, res) {
+  const restaurantId = req.params.id;
+  res.render('restaurant-detail', { rid: restaurantId })
+});
+
 app.get('/recommend', function(req, res) {
   res.render('recommend');
 });
 
 app.post('/recommend', function(req, res) {
   const restaurants = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
